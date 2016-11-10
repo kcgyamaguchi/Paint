932277fedc9c442ebba4057b6ff3ac180befab32
kcgyamaguchi <lyese7705@gmail.com>
2016-11-10 15:35:25 +0900

    fontFamilyの実装 fontWeightの実装 textDecorationの実装 Textalignの実装 不要なコードの削除

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ window.onload = function() {
     $(window).keyup(function (e) {
         // デリートキーが押されたら、図形の削除を行う
         if (e.keyCode === 46) {
-            gui.remove();
+            gui.ObjectRemove();
         }
     });
 
@@ -29,36 +29,41 @@ window.onload = function() {
     // 初期設定
     var gui = new dat.GUI();
 
-        // Canvas
-        gui.BackgroundColor = "#FFFFFF";
-        gui.Zoomrate = 100;
+    // Canvas
+    gui.BackgroundColor = "#FFFFFF";
+    gui.Zoomrate = 100;
 
-        // Brush
-        gui.brushType = "InkBrush";
-        gui.brushWidth = 30;
-        gui.brushOpacity = 1;
-        gui.inkAmount = 7;
-        gui.brushColor = "#000000";
-        gui.BackgroundColor = "#FFFFFF";
+    // Brush
+    gui.brushType = "InkBrush";
+    gui.brushWidth = 30;
+    gui.brushOpacity = 1;
+    gui.inkAmount = 7;
+    gui.brushColor = "#000000";
 
-        // Objdrawing
-        gui.objbrushType = "Pencil";
-        gui.Linecolor = "#000000";
-        gui.Linewidth = 1;
-        gui.Shadowcolor = "#000000";
-        gui.Shadowwidth = 1;
-        gui.Shadowoffset = 1;
+    // Objdrawing
+    gui.objbrushType = "Pencil";
+    gui.Linecolor = "#000000";
+    gui.Linewidth = 1;
+    gui.Shadowcolor = "#000000";
+    gui.Shadowwidth = 1;
+    gui.Shadowoffset = 1;
        
-        // Text
-        gui.text = "Text";
-        gui.TextColor = "#000000";
-        gui.TextSize = 100;
+    // Text
+    gui.text = "Text";
+    gui.TextColor = "#000000";
+    gui.fontweight = "normal";
+    gui.textDecoration = "normal";
+    gui.fontFamily = "arial";
+    gui.TextSize = 50;
+    gui.textstroke = "#000000";
+    gui.textstrokeWidth = 1;
+    gui.textAlign = "left";
 
-        // Graphic
-        gui.GraphicColor = "#000000";
-        gui.StrokeColor = "#000000";
-        gui.strokeWidth = 3;
-        gui.radius = 5;
+    // Graphic
+    gui.GraphicColor = "#000000";
+    gui.StrokeColor = "#000000";
+    gui.strokeWidth = 3;
+    gui.radius = 5;
 
     // Brushの初期化
     setupBrush(gui.brushType, {
@@ -74,10 +79,11 @@ window.onload = function() {
     }
 
     // Canvasのクリア
-    gui.clear = function () {
+    gui.Clear = function () {
         canvas.isDrawingMode = false;
         if (confirm('本当にクリアしますか？')) {
             canvas.clearContext(canvas.contextTop);
+            canvas.clear();
         }
     }
 
@@ -101,21 +107,25 @@ window.onload = function() {
 
         canvas.observe('mouse:down', function (e) {
 
-        mouse_pos = canvas.getPointer(e.e);
-        size = parseInt(size, 10);
+            mouse_pos = canvas.getPointer(e.e);
+            size = parseInt(size, 10);
 
-        canvas.add(new fabric.Text(gui.text, {
-            fontSize: gui.TextSize,
-            left: mouse_pos.x,
-            top: mouse_pos.y,
-            textAlign: "left",
-            fontWeight: 'bold',
-            fill: gui.TextColor,
-        }));
+            canvas.add(new fabric.Text(gui.text, {
+                fontSize: gui.TextSize,
+                left: mouse_pos.x,
+                top: mouse_pos.y,
+                textAlign: gui.textAlign,
+                fontWeight: gui.fontweight,
+                fontFamily: gui.fontFamily,
+                textDecoration: gui.textDecoration,
+                fill: gui.TextColor,
+                stroke: gui.textstroke,
+                strokeWidth: gui.textstrokeWidth,
+            }));
 
-        canvas.off('mouse:down');
-        canvas.renderAll();
-        canvas.calcOffset();
+            canvas.off('mouse:down');
+            canvas.renderAll();
+            canvas.calcOffset();
         });
     }
 
@@ -124,18 +134,18 @@ window.onload = function() {
         var mouse_pos = { x: 0, y: 0 };
         canvas.isDrawingMode = false;
         canvas.observe('mouse:down', function (e) {
-        mouse_pos = canvas.getPointer(e.e);
-        canvas.add(new fabric.Rect({
-            left: mouse_pos.x,
-            top: mouse_pos.y,
-            width: 75,
-            height: 50,
-            fill: gui.GraphicColor,
-            stroke: gui.StrokeColor,
-            strokeWidth: gui.strokeWidth,
-            padding: gui.radius
-        }));
-        canvas.off('mouse:down');
+            mouse_pos = canvas.getPointer(e.e);
+            canvas.add(new fabric.Rect({
+                left: mouse_pos.x,
+                top: mouse_pos.y,
+                width: 75,
+                height: 50,
+                fill: gui.GraphicColor,
+                stroke: gui.StrokeColor,
+                strokeWidth: gui.strokeWidth,
+                padding: gui.radius
+            }));
+            canvas.off('mouse:down');
         });
     }
 
@@ -146,14 +156,14 @@ window.onload = function() {
         canvas.observe('mouse:down', function (e) {
             mouse_pos = canvas.getPointer(e.e);
             canvas.add(new fabric.Circle({
-            left: mouse_pos.x,
-            top: mouse_pos.y,
-            radius: gui.radius,
-            fill: gui.GraphicColor,
-            stroke: gui.StrokeColor,
-            strokeWidth: gui.strokeWidth,
-        }));
-        canvas.off('mouse:down');
+                left: mouse_pos.x,
+                top: mouse_pos.y,
+                radius: gui.radius,
+                fill: gui.GraphicColor,
+                stroke: gui.StrokeColor,
+                strokeWidth: gui.strokeWidth,
+            }));
+            canvas.off('mouse:down');
         });
     }
 
@@ -166,15 +176,15 @@ window.onload = function() {
         canvas.observe('mouse:down', function (e) {
             mouse_pos = canvas.getPointer(e.e);
             canvas.add(new fabric.Ellipse({
-            rx: 45,
-            ry: 25,
-            fill: gui.GraphicColor,
-            stroke: gui.StrokeColor,
-            strokeWidth: gui.strokeWidth,
-            left: mouse_pos.x,
-            top: mouse_pos.y
-        }));
-        canvas.off('mouse:down');
+                rx: 45,
+                ry: 25,
+                fill: gui.GraphicColor,
+                stroke: gui.StrokeColor,
+                strokeWidth: gui.strokeWidth,
+                left: mouse_pos.x,
+                top: mouse_pos.y
+            }));
+            canvas.off('mouse:down');
         });
     }
 
@@ -243,16 +253,8 @@ window.onload = function() {
         window.open(dataURL);
     }
 
-    // Canvasをクリアする
-    gui.Clear = function () {
-        canvas.isDrawingMode = false;
-        if (confirm('本当にクリアしますか？')) {
-            canvas.clear();
-        }
-    }
-
     // オブジェクトを削除する
-    gui.remove = function () {
+    gui.ObjectRemove = function () {
         canvas.isDrawingMode = false;
 
         var activeObject = canvas.getActiveObject(),
@@ -264,21 +266,24 @@ window.onload = function() {
         }　else if (activeGroup) {
             if (confirm('本当に削除しますか？')) {
                 var objectsInGroup = activeGroup.getObjects();
-                    canvas.discardActiveGroup();
-                    objectsInGroup.forEach(function (object) {
-                        canvas.remove(object);
-                    });
-                }
+                canvas.discardActiveGroup();
+                objectsInGroup.forEach(function (object) {
+                    canvas.remove(object);
+                });
             }
+        }
     };
 
     // メニュー
-    var file = gui.addFolder("File");
-    file.add(gui, "Clear");
-    file.add(gui, "Save");
+    {
+        var file = gui.addFolder("File");
+        file.add(gui, "Clear");
+        file.add(gui, "Save");
+    }
 
     // Canvasメニュー
-    var f1 = gui.addFolder("Canvas");
+    {
+        var f1 = gui.addFolder("Canvas");
         f1.add(gui, 'Zoomrate', 1, 200).onChange(function (value) {
             if (value > 100) {
                 canvas.setZoom(value/ 100);
@@ -287,15 +292,17 @@ window.onload = function() {
             }
         });
 
-        f1.add(gui, "remove");
+        f1.add(gui, "ObjectRemove");
         f1.addColor(gui, "BackgroundColor")
             .onChange(function (value) {
                 canvas.setBackgroundColor(value);
                 canvas.renderAll();
             });
+    }
 
     // メニュー
-    var f0 = gui.addFolder('Free drawing');
+    {
+        var f0 = gui.addFolder('Free drawing');
         f0.add(gui, "FreeDrawing");
         f0.add(gui, "brushType", ["CrayonBrush", "InkBrush", "MarkerBrush", "SprayBrush"]).onFinishChange(setupBrush);
         f0.add(gui, "brushWidth", 1, 100).step(1)
@@ -309,20 +316,22 @@ window.onload = function() {
                 canvas.freeDrawingBrush.changeColor(value);
             });
 
-        // 
+        // brushOpacity
         f0.add(gui, "brushOpacity", 0.1, 1).step(0.1)
             .onChange(function (value) {
                 canvas.freeDrawingBrush.changeOpacity(value);
             });
 
-        // 
+        // inkAmount
         f0.add(gui, "inkAmount", 1, 10).step(0.1)
               .onChange(function (value) {
                   canvas.freeDrawingBrush.inkAmount = value;
               });
+    }
 
     // オブジェクトドローイング
-    var ojbdr = gui.addFolder("ObjectModeDrawing");
+    {
+        var ojbdr = gui.addFolder("ObjectModeDrawing");
         ojbdr.add(gui, "objbrushType", ["Pencil", "Circle"]).onChange(function (value) {
             canvas.isDrawingMode = true;
             canvas.freeDrawingBrush = new fabric[value + 'Brush'](canvas);
@@ -335,35 +344,49 @@ window.onload = function() {
         ojbdr.addColor(gui, "Linecolor")
             .onChange(function (value) {
                 canvas.freeDrawingBrush.color = value;
-        });
-
+            });
+    }
 
     // Add:Text
-    var f2 = gui.addFolder('Text');
-        f2.add(gui, "text_input");
-        f2.add(gui, 'text');
-        f2.add(gui, 'TextSize', 1, 100); // Min and max
-        f2.addColor(gui, "TextColor")
-            .onChange(function (value) {
-            gui.TextColor = value;
-        });
+    {
+        var f2 = gui.addFolder('Text');
+            f2.add(gui, "text_input");
+            f2.add(gui, 'text');
+            f2.add(gui, 'TextSize', 1, 100);
+            f2.add(gui, "fontFamily", ["arial", "helvetica", "myriad pro", "delicious", "verdana", "georgia", "courier", "comic sans ms", "impact", "monaco", "optima", "hoefler text", "plaster", "engagement"])
+            f2.addColor(gui, "TextColor")
+                .onChange(function (value) {
+                gui.TextColor = value;
+                });
+            f2.add(gui, "fontweight", ["normal", "Bold", "Italic"])
+            f2.add(gui, "textDecoration", ["normal", "underline", "line-through", "overline"])
+
+            f2.addColor(gui, "textstroke")
+                .onChange(function (value) {
+                    gui.textstroke = value;
+                });
+            f2.add(gui, 'textstrokeWidth', 1, 100);
+            f2.add(gui, "textAlign", ["left", "center", "right"])
+    }
 
     // Graphic
-    var f3 = gui.addFolder('Graphic');
-        f3.add(gui, 'rect');
-        f3.add(gui, 'circle');
-        f3.add(gui, 'ellipse');
-        f3.add(gui, 'line');
-        f3.addColor(gui, "GraphicColor")
-            .onChange(function (value) {
-            gui.GraphicColor = value;
-        });
+    {
+        var f3 = gui.addFolder('Graphic');
+            f3.add(gui, 'rect');
+            f3.add(gui, 'circle');
+            f3.add(gui, 'ellipse');
+            f3.add(gui, 'line');
+            f3.addColor(gui, "GraphicColor")
+                .onChange(function (value) {
+                gui.GraphicColor = value;
+            });
 
-        f3.addColor(gui, "StrokeColor")
-            .onChange(function (value) {
-            gui.StrokeColor = value;
-        });
+            f3.addColor(gui, "StrokeColor")
+                .onChange(function (value) {
+                gui.StrokeColor = value;
+            });
 
-        f3.add(gui, 'strokeWidth', 1, 100);
-        f3.add(gui, 'radius', 1, 100);
+            f3.add(gui, 'strokeWidth', 1, 100);
+            f3.add(gui, 'radius', 1, 100);
+    }
 };
